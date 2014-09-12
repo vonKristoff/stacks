448c7adbaa373bba6d9911a1d90a4b73c056c52b
Vonkristoff <mrjcnicolas@gmail.com>
2014-09-12 21:26:34 +0100

    First Stack script created.

---
+++ nodestack.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+## 
+# Install scripts for the following service daemons
+##
+
+# Install NGINX
+apt-get -y install nginx
+
+# Install Node
+apt-get -y install nodejs
+
+# Install NPM for Node
+apt-get -y install npm
+
+# Install CouchDB
+apt-get -y install couchdb
